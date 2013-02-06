f79a37e4d617a226c491517d3c2016273c7b3077
no-cannabis <no.cannabis@gmail.com>
2013-02-06 10:52:36 -0500

    fixed case statements in recovery_keys.c
    Change-Id: Ieeae8276ad4501dacd68d8aeab1923a1e360b315

--- recovery_keys.c
+++ recovery_keys.c
@@ -12,24 +12,14 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
-	    case 11:
 	    case 114:
                 return HIGHLIGHT_DOWN;
 
             case 115:
-	    case 1:
                 return HIGHLIGHT_UP;
 
-            case 102:
-            case KEY_POWER:
+            case 116:
                 return SELECT_ITEM;
-            
-	    case 158:
-	    case KEY_1:
-            case KEY_ESC:
-                if (ui_menu_level > 0) {
-                    return GO_BACK;
-                }
         }
     }
 
